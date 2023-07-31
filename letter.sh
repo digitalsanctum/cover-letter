262991d0748b8205be499601b34616cafbee281d
Shane Witbeck <shane@digitalsanctum.com>
2023-07-31 09:27:16 -0700

    feat: remove makefile, output to company dir

--- letter.sh
+++ letter.sh
@@ -17,26 +17,30 @@ echo "Encoded company name: $ENCODED_COMPANY_NAME"
 
 # convert company_name to valid file name with json suffix
 COMPANY_PREFIX=$(printf %s "$COMPANY" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '[:punct:]')
-COMPANY_FILE="$COMPANY_PREFIX.json"
+
+mkdir -p $COMPANY_PREFIX
+
+COMPANY_FILE="$COMPANY_PREFIX/company.json"
 echo "Company file: $COMPANY_FILE"
 
-# given the company name, curl the local service to get the address
-CA_URL="http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address"
-echo "Getting company address from $CA_URL"
-COMPANY_RESPONSE=$(curl -s "http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address" | jq -r .)
-if [ $? -ne 0 ]; then
-    echo "Error: could not get mailing address for $COMPANY"
-    exit 1
-fi
+function get_company_address() {
+  # given the company name, curl the local service to get the address
+      CA_URL="http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address"
+      echo "Getting company address from $CA_URL"
+      COMPANY_RESPONSE=$(curl -s "$CA_URL" | jq -r .)
+      if [ $? -ne 0 ]; then
+          echo "Error: could not get mailing address for $COMPANY"
+          exit 1
+      fi
 
-COMPANY_NAME=$(echo $COMPANY_RESPONSE | jq -r '.company_name')
-COMPANY_ADDRESS=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.street')
-COMPANY_CITY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.city')
-COMPANY_STATE=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.state_or_province')
-COMPANY_ZIP=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.postal_code')
-COMPANY_COUNTRY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.country')
+      COMPANY_NAME=$(echo $COMPANY_RESPONSE | jq -r '.company_name')
+      COMPANY_ADDRESS=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.street')
+      COMPANY_CITY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.city')
+      COMPANY_STATE=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.state_or_province')
+      COMPANY_ZIP=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.postal_code')
+      COMPANY_COUNTRY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.country')
 
-# create a json file with the company name, role and location
+      # create a json file with the company name, role and location
 cat > $COMPANY_FILE <<EOF
 {
     "company_name": "$COMPANY_NAME",
@@ -47,31 +51,35 @@ cat > $COMPANY_FILE <<EOF
     "company_country": "$COMPANY_COUNTRY"
 }
 EOF
+}
+
+# check if company file exists
+if [ -f "$COMPANY_FILE" ]; then
+    echo "Company file $COMPANY_FILE already exists; skipping lookup"
+else
+    get_company_address
+fi
 
 TEMPLATE_FILE="letter.md.mustache"
-MARKDOWN_OUTPUT_FILE="letter.md"
-PDF_OUTPUT_FILE="output.pdf"
-FONT="Hoefler Text"
-ALT_FONT="Helvetica Neue"
-MONO_FONT="Courier"
-FONT_SIZE="11pt"
-GEOMETRY="a4paper, left=35mm, right=35mm, top=25mm, bottom=25mm"
-LANG="en-US"
 
+AUTHOR_FILE="author.json"
+CONFIG_FILE="config.json"
+MARKDOWN_OUTPUT_FILE="${COMPANY_PREFIX}/letter.md"
+POSITION_FILE="${COMPANY_PREFIX}/position.json"
+FINAL_DATA_FILE="${COMPANY_PREFIX}/data.json"
+FINAL_PDF_FILE="${COMPANY_PREFIX}/final.pdf"
 
-cat > position.json <<EOF
+cat > ${POSITION_FILE} <<EOF
 {
     "company_focus": "$FOCUS",
     "position_title": "$ROLE"
 }
 EOF
 
-rm -rf combined.json
-
-FINAL_DATA_FILE="combined.json"
+rm -rf $FINAL_DATA_FILE
 
 # combine data_file and author.json file
-cat author.json config.json position.json $COMPANY_FILE | jq -s add | tee -a $FINAL_DATA_FILE
+cat $AUTHOR_FILE $CONFIG_FILE $POSITION_FILE $COMPANY_FILE | jq -s add | tee $FINAL_DATA_FILE
 
 # check if data file exists
 if [ ! -f "$FINAL_DATA_FILE" ]; then
@@ -79,11 +87,10 @@ if [ ! -f "$FINAL_DATA_FILE" ]; then
     exit 1
 fi
 
-cat $FINAL_DATA_FILE | jq .
-
+# first, use mustache to generate the letter.md file
 pbl -d $FINAL_DATA_FILE -t $TEMPLATE_FILE > $MARKDOWN_OUTPUT_FILE
 
-make
+pandoc $MARKDOWN_OUTPUT_FILE -o $FINAL_PDF_FILE --template=template.tex --pdf-engine=xelatex
 
-open $PDF_OUTPUT_FILE
+open $FINAL_PDF_FILE
 
