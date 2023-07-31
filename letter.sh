e6386f7aff3a34c16d995a82a3c7cba87bdcea44
Shane Witbeck <shane@digitalsanctum.com>
2023-07-31 08:42:47 -0700

    feat: further split data files

--- letter.sh
+++ letter.sh
@@ -8,37 +8,46 @@ if [ $# -ne 3 ]; then
     exit 1
 fi
 
-COMPANY_NAME=$1
+COMPANY=$1
 FOCUS=$2
 ROLE=$3
 
-ENCODED_COMPANY_NAME=$(printf %s "$COMPANY_NAME" |jq -sRr @uri)
+ENCODED_COMPANY_NAME=$(printf %s "$COMPANY" |jq -sRr @uri)
 echo "Encoded company name: $ENCODED_COMPANY_NAME"
 
 # convert company_name to valid file name with json suffix
-DATA_FILE=$(printf %s "$COMPANY_NAME" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '[:punct:]').json
-echo "Data file: $DATA_FILE"
+COMPANY_PREFIX=$(printf %s "$COMPANY" | tr '[:upper:]' '[:lower:]' | tr ' ' '-' | tr -d '[:punct:]')
+COMPANY_FILE="$COMPANY_PREFIX.json"
+echo "Company file: $COMPANY_FILE"
 
 # given the company name, curl the local service to get the address
 CA_URL="http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address"
 echo "Getting company address from $CA_URL"
-COMPANY_MAILING_ADDRESS=$(curl -s "http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address" | jq -r .mailing_address)
+COMPANY_RESPONSE=$(curl -s "http://localhost:8000/companies/$ENCODED_COMPANY_NAME/address" | jq -r .)
 if [ $? -ne 0 ]; then
-    echo "Error: could not get mailing address for $COMPANY_NAME"
+    echo "Error: could not get mailing address for $COMPANY"
     exit 1
 fi
-echo $COMPANY_MAILING_ADDRESS | jq .
-COMPANY_ADDRESS=$(echo $COMPANY_MAILING_ADDRESS | jq -r '.street')
-COMPANY_CITY=$(echo $COMPANY_MAILING_ADDRESS | jq -r '.city')
-COMPANY_STATE=$(echo $COMPANY_MAILING_ADDRESS | jq -r '.state_or_province')
-COMPANY_ZIP=$(echo $COMPANY_MAILING_ADDRESS | jq -r '.postal_code')
-COMPANY_COUNTRY=$(echo $COMPANY_MAILING_ADDRESS | jq -r '.country')
 
-AUTHOR="Shane Witbeck"
-AUTHOR_CITY="Kalama"
-AUTHOR_STATE="Washington"
-AUTHOR_EMAIL="shane.witbeck@gmail.com"
-AUTHOR_PHONE="+1 (503) 956-0782"
+COMPANY_NAME=$(echo $COMPANY_RESPONSE | jq -r '.company_name')
+COMPANY_ADDRESS=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.street')
+COMPANY_CITY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.city')
+COMPANY_STATE=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.state_or_province')
+COMPANY_ZIP=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.postal_code')
+COMPANY_COUNTRY=$(echo $COMPANY_RESPONSE | jq -r '.mailing_address.country')
+
+# create a json file with the company name, role and location
+cat > $COMPANY_FILE <<EOF
+{
+    "company_name": "$COMPANY_NAME",
+    "company_address": "$COMPANY_ADDRESS",
+    "company_city": "$COMPANY_CITY",
+    "company_state": "$COMPANY_STATE",
+    "company_zip": "$COMPANY_ZIP",
+    "company_country": "$COMPANY_COUNTRY"
+}
+EOF
+
 TEMPLATE_FILE="letter.md.mustache"
 MARKDOWN_OUTPUT_FILE="letter.md"
 PDF_OUTPUT_FILE="output.pdf"
@@ -49,23 +58,10 @@ FONT_SIZE="11pt"
 GEOMETRY="a4paper, left=35mm, right=35mm, top=25mm, bottom=25mm"
 LANG="en-US"
 
-rm -rf $DATA_FILE
 
-# create a json file with the company name, role and location
-cat > $DATA_FILE <<EOF
+cat > position.json <<EOF
 {
-    "geometry": "$GEOMETRY",
-    "font": "$FONT",
-    "alt_font": "$ALT_FONT",
-    "mono_font": "$MONO_FONT",
-    "font_size": "$FONT_SIZE",
-    "company": "$COMPANY_NAME",
     "company_focus": "$FOCUS",
-    "company_address": "$COMPANY_ADDRESS",
-    "company_city": "$COMPANY_CITY",
-    "company_state": "$COMPANY_STATE",
-    "company_zip": "$COMPANY_ZIP",
-    "company_country": "$COMPANY_COUNTRY",
     "position_title": "$ROLE"
 }
 EOF
@@ -75,20 +71,17 @@ rm -rf combined.json
 FINAL_DATA_FILE="combined.json"
 
 # combine data_file and author.json file
-cat author.json $DATA_FILE | jq -s add | tee -a $FINAL_DATA_FILE
-cat $FINAL_DATA_FILE | jq .
+cat author.json config.json position.json $COMPANY_FILE | jq -s add | tee -a $FINAL_DATA_FILE
 
 # check if data file exists
 if [ ! -f "$FINAL_DATA_FILE" ]; then
-    echo "Error: data file combined.json does not exist"
+    echo "Error: data file $FINAL_DATA_FILE does not exist"
     exit 1
 fi
 
 cat $FINAL_DATA_FILE | jq .
 
 pbl -d $FINAL_DATA_FILE -t $TEMPLATE_FILE > $MARKDOWN_OUTPUT_FILE
-
-# pandoc --template=default.latex --pdf-engine=xelatex -s $MARKDOWN_OUTPUT_FILE -o $PDF_OUTPUT_FILE
 
 make
 
